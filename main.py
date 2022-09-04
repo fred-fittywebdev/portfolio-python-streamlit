3757b9f4036e9d7cc32ef7500f69ca876806eb0d
fred-fittywebdev <contact@fittywebdev.fr>
2022-09-04 13:57:15 +0200

    update project link

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
     st.download_button(
-        label="📄 boxDownload Resume",
+        label="📄 Download Resume",
         data=PDFByte,
         file_name=resume_file.name,
         mime="application/octet-stream",
